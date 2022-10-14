5514d299809afcaef93499f498058ec2a41f4baf
Daniel <daniel.araujo.dev@hotmail.com>
2022-10-13 23:15:28 -0300

    Implementação dos métodos: CadastrarHospedes, ObterQuantidadeHospedes e CalcularValorDiaria.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,4 @@
+using DesafioProjetoHospedagem;
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -17,12 +18,14 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            int capacidade = hospedes.Count;
+            if (Suite.Capacidade >= capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
+                throw new SystemException($"Desculpe reserva indisponível, a capacidade de reserva {Suite.Capacidade} é menor do que o número de hóspedes {hospedes.Count}.");
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
             }
@@ -37,7 +40,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +48,14 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            const decimal desconto = 0.1M;
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= valor * desconto;
             }
 
             return valor;
